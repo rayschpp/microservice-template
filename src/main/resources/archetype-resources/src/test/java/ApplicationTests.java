0c8ca2f9a62b716b1abfcb2bebeab09191ba3c28
Raynhold Schapp <23237786+rayschpp@users.noreply.github.com>
2020-11-05 11:06:32 +0100

    [#4] Adds unit test for the context load of the required instances

--- src/main/resources/archetype-resources/src/test/java/ApplicationTests.java
+++ src/main/resources/archetype-resources/src/test/java/ApplicationTests.java
@@ -1,16 +1,35 @@
 package $package;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import ${package}.assembler.${entity}ModelAssembler;
+import ${package}.controller.${entity}Controller;
+import ${package}.repository.${entity}Repository;
+import ${package}.service.I${entity}Service;
+
 @SpringBootTest
 class ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private ${entity}ModelAssembler assembler;
+    @Autowired
+    private ${entity}Controller controller;
+    @Autowired
+    private ${entity}Repository repository;
+    @Autowired
+    private I${entity}Service service;
+
+    @Test
+    void contextLoads() {
+	assertThat(assembler).isNotNull();
+	assertThat(controller).isNotNull();
+	assertThat(repository).isNotNull();
+	assertThat(service).isNotNull();
+    }
 
 }
